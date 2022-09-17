5a477028402c501a43b1371b2d3e175786916c67
thecodedot <72885755+thecodedot@users.noreply.github.com>
2022-09-18 03:42:08 +0500

    grid:true

--- js/app.js
+++ js/app.js
@@ -25,49 +25,57 @@ var Bedtime_consistency_data = [
 SE_data = {
   x: ['2000/01/01-2000/01/10', '2000/01/12-2000/01/15', '2000/01/13-2000/01/17'],
   y: [2.5, 3.7, 1],
+  title: 'SE_data',
   name: 'SE_data',
   type: 'scatter',
   opacity: 1,
   line: {
     color: 'red',
     size: [20, 20, 20, 20]
-  }
+  },
+  position: 'top',
 };
 
 TST_data = {
   x: ['2000/01/01-2000/01/10', '2000/01/12-2000/01/15', '2000/01/13-2000/01/17'],
   y: [4, 5, 9],
+  title: 'TST_data',
   name: 'TST_data',
   type: 'scatter',
   opacity: 1,
   line: {
     color: 'purple',
     size: [20, 20, 20, 20]
-  }
+  },
+  position: 'top',
 };
 
 EDS_data = {
   x: ['2000/01/01-2000/01/10', '2000/01/12-2000/01/15', '2000/01/13-2000/01/17'],
   y: [22, 21, 24],
+  title: 'EDS_data',
   name: 'EDS_data',
   type: 'scatter',
   opacity: 1,
   line: {
     color: 'green',
     size: [20, 20, 20, 20]
-  }
+  },
+  position: 'top',
 };
 
 Bedtime_consistency_data = {
   x: ['2000/01/01-2000/01/10', '2000/01/12-2000/01/15', '2000/01/13-2000/01/17'],
   y: [70, 81, 71],
+  title: 'Bedtime_consistency_data',
   name: 'Bedtime_consistency_data',
   type: 'scatter',
   opacity: 1,
   line: {
     color: 'blue',
     size: [20, 20, 20, 20]
-  }
+  },
+  position: 'top',
 };
 
 var layout = {
@@ -89,6 +97,8 @@ var layout = {
         color: '#7f7f7f'
       }
     },
+    showgrid:true,
+    zeroline: false
   },
   yaxis: {
     title: {
@@ -98,8 +108,10 @@ var layout = {
         size: 18,
         color: '#7f7f7f'
       },
-    }
-  }
+    },
+    showgrid:false,
+    zeroline: true
+  },
 };
 
 var data = [SE_data, TST_data, EDS_data, Bedtime_consistency_data];
